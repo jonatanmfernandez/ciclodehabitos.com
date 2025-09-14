f4c9586b367ae5cd169981b88996a60e4861768e
Jonatan Fernandez <fernandezjonatan.m@gmail.com>
2025-09-13 22:57:43 -0300

    Fix consts.ts with proper Spanish characters

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Ciclo de Habitos';
-export const SITE_DESCRIPTION = 'Blog sobre hábitos positivos';
+export const SITE_TITLE = 'Ciclo de Hábitos';
+export const SITE_DESCRIPTION = 'Transforma tu rutina, transforma tus resultados. El espacio donde el desarrollo personal se convierte en un viaje tangible y transformador.';
